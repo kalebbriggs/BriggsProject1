49280e36279618beafc19fa0a2727d3d0a05b2a8
kalebbriggs <92959033+kalebbriggs@users.noreply.github.com>
2021-10-22 01:19:48 -0500

    Create Systemaudit.sh

---
+++ Linux/BashScripts/Systemaudit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Code creates a system audit providing information about the current system 
+
+echo "A Quick System Audit Script"
+date
+
+echo ""
+echo "Machine Type Info:"
+
+echo $MACHTYPE
+echo -e "Uname info: $(uname -a) \n"
+
+echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n"
+echo "Hostname: $(hostname -s) "
